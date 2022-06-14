e3fa1adeaaf4c5ac7d501c001ed1d2034a408c43
MateuszCzech11 <92048729+MateuszCzech11@users.noreply.github.com>
2022-06-14 08:28:15 +0200

    2022-06-14 MC fix

--- app/models/opinion.py
+++ app/models/opinion.py
@@ -1,21 +1,21 @@
-from app.parameters import selectors
 from app.utils import get_item
+from app.parameters import selectors
 
 class Opinion:
-    def __init__(self, author="",recommendation=None, stars=0, content="", useful=0,useless=0, publish_date=None, purchase_date=None,pros=[],cons=[],opinion_id=""):
+    def __init__(self, author="", recommendation=None, stars=0, content="", useful=0, useless=0, publish_date=None, purchase_date=None, pros=[], cons=[], opinion_id=""):
         self.author = author
-        self.recommendation=recommendation
-        self.stars=stars
-        self.content=content
-        self.useful=useful
-        self.useless=useless
-        self.publish_date=publish_date
-        self.purchase_date=purchase_date
-        self.pros=pros
-        self.cons=cons
-        self.opinion_id=opinion_id
+        self.recommendation = recommendation
+        self.stars = stars
+        self.content = content
+        self.useful = useful
+        self.useless = useless
+        self.publish_date = publish_date
+        self.purchase_date = purchase_date
+        self.pros = pros
+        self.cons = cons
+        self.opinion_id = opinion_id
         return self
-
+    
     def __str__(self):
         pass
 
@@ -25,6 +25,8 @@ class Opinion:
     def to_dict(self):
         pass
 
-    def extract_opinions(self,opinion):
-        for key,value in opinion:
-            pass
+    def extract_opinion(self, opinion):
+        for key, value in selectors.items():
+            setattr(self, key, get_item(opinion, *value))
+        self.opinion_id = opinion["data-entry-id"]             
+        return self
